c96cd72cdf9bd40a4dcda2eb07fbe8087988124d
Eduardo Pereira <silvapereiraeduardo@gmail.com>
2019-10-02 12:38:19 -0300

    Aula 3 - Frontend e ajustes no Backend

--- aulas/backend/src/server.js
+++ aulas/backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
 const routes = require("./routes");
 
 const app = express();
@@ -18,7 +21,9 @@ mongoose.connect(
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
 
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
