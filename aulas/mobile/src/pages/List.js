0e8baf2ce79af37951d0ec9306731ec6f4de6974
Eduardo Pereira <silvapereiraeduardo@gmail.com>
2019-10-03 14:17:50 -0300

    Mobile

---
+++ aulas/mobile/src/pages/List.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import {
+  AsyncStorage,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  Image
+} from "react-native";
+
+import SpotList from "../components/SpotList";
+
+import logo from "../assets/logo.png";
+
+export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("techs").then(storageTechs => {
+      const techsArray = storageTechs.split(",").map(tech => tech.trim());
+      setTechs(techsArray);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+      <ScrollView>
+        {techs.map((tech, idx) => (
+          <SpotList key={idx} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 10
+  }
+});
